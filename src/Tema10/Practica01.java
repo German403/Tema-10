f40de37ac55ddddd99ac7e69d2f013765bfdb71b
alumno <gescuderodriguez@gmail.com>
2024-03-21 13:27:17 +0100

    Ejercicios del tema 10

--- src/Tema10/Practica01.java
+++ src/Tema10/Practica01.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Practica01 {
 
+    public static void desplazar(ArrayList<Integer> l){
+        l.add(0, 1.remove(1.size()-1));
+    }
     public static void main(String[] args) {
         ArrayList<Integer> orden = new ArrayList<>();
         orden.add(13);
@@ -13,7 +16,6 @@ public class Practica01 {
         orden.add(12);
         System.out.println("Lista de numeros original");
         System.out.println(orden);
-
 
     }
 }
